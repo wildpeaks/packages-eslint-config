55d5145ef280435c1b05d42522519ddc371d40fe
Cecile Muller <git@wildpeaks.fr>
2017-04-05 15:46:10 +0200

    Disabled "flowtype/require-variable-type"
    That rule seems to run even on files it's not meant to lint, so I had to
    force-disable it on false positives for now

--- test/fixtures/quotes_property_inconsistent_single.js
+++ test/fixtures/quotes_property_inconsistent_single.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-implicit-globals */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-var */
+/* eslint-disable flowtype/require-variable-type */
 
 var myobj = {
 	myprop: 1,
