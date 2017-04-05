c10d277a0115abab7c8412739c04c865f2e1d79c
Cecile Muller <git@wildpeaks.fr>
2017-04-05 15:41:12 +0200

    Replaced esmodule test with more detailed ones

---
+++ test/fixtures/export_default_arrow.js
@@ -0,0 +1,9 @@
+/* eslint-disable strict */
+/* eslint-disable arrow-body-style */
+/* eslint-disable space-before-blocks */
+/* eslint-disable flowtype/require-variable-type */
+/* eslint-disable flowtype/require-return-type */
+
+export default () => {
+	return Date.now();
+};
---
+++ test/fixtures/export_default_const.js
@@ -0,0 +1,4 @@
+/* eslint-disable strict */
+/* eslint-disable flowtype/require-variable-type */
+
+export default const myvalue = 123;
