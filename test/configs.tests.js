6ecabebd772c9dfa42859bf5efe89dae7a244aa3
Cecile Muller <git@wildpeaks.fr>
2017-11-10 23:35:51 +0100

    Added the "concatenate" fixtures to the tests suite

--- test/configs.tests.js
+++ test/configs.tests.js
@@ -115,6 +115,14 @@ function testPackage(packageId, done){
 		quotes_property_double: ['quotes'],
 		quotes_double: ['quotes'],
 
+		quotes_concatenate_number_number: [],
+		quotes_concatenate_number_string_single: es2017 ? ['prefer-template'] : [],
+		quotes_concatenate_number_string_double: es2017 ? ['prefer-template', 'quotes'] : ['quotes'],
+		quotes_concatenate_number_string_backtick: es2017 ? [] : ['fatal'],
+		quotes_concatenate_string_string_single: es2017 ? ['prefer-template'] : [],
+		quotes_concatenate_string_string_double: es2017 ? ['prefer-template', 'quotes'] : ['quotes'],
+		quotes_concatenate_string_string_backtick: es2017 ? [] : ['fatal'],
+
 		chained_two_methods_single_line: [],
 		chained_two_methods_multiple_lines: [],
 		chained_four_methods_single_line: [],
