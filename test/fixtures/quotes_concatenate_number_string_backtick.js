c835c0068662cc36ee26545c4f9a63f867324cd0
Cecile Muller <git@wildpeaks.fr>
2017-11-10 23:10:25 +0100

    More fixtures for testing concatenation

---
+++ test/fixtures/quotes_concatenate_number_string_backtick.js
@@ -0,0 +1,7 @@
+/* eslint-disable strict */
+/* eslint-disable no-implicit-globals */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-var */
+
+var myvar1 = 123;
+var myvar2 = `${myvar1} world`;
