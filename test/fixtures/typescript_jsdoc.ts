bda2b7992b36cee6c3d045551db9f2b675efcabc
Cecile Muller <git@wildpeaks.fr>
2017-12-27 17:06:18 +0100

    Added a fixture for the `@param` jsdoc statement, in case the ESlint TS parser ever fixes the false positive one day

---
+++ test/fixtures/typescript_jsdoc.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+
+/**
+ * Description of function
+ * @param myparam Description of myparam
+ * @returns Description of result
+ */
+function myfunction(myparam: number): number {
+	return myparam + 1;
+}
