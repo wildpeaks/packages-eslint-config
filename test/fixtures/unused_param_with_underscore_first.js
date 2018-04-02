f09a23463b4c89c7f6f762f077e98a028cae3aac
Cecile Muller <git@wildpeaks.fr>
2018-04-02 03:19:49 +0200

    Added a few test for `no-unused-vars`

---
+++ test/fixtures/unused_param_with_underscore_first.js
@@ -0,0 +1,10 @@
+/* global dummy */
+/* eslint-disable strict */
+/* eslint-disable space-before-blocks */
+'use strict';
+
+function myfunction(_param1, param2){
+	return param2 + 1;
+}
+
+dummy(myfunction);
---
+++ test/fixtures/unused_param_with_underscore_last.js
@@ -0,0 +1,10 @@
+/* global dummy */
+/* eslint-disable strict */
+/* eslint-disable space-before-blocks */
+'use strict';
+
+function myfunction(param1, _param2){
+	return param1 + 1;
+}
+
+dummy(myfunction);
