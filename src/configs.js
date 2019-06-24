4ad12dfb3e2b1dea785e3faf06299216ff25708c
Cecile Muller <git@wildpeaks.fr>
2019-06-24 16:20:26 +0200

    Removed "react" and "stage2" configs

--- src/configs.js
+++ src/configs.js
@@ -4,7 +4,6 @@ module.exports = {
 	'eslint-config-legacy': {
 		name: 'ES5 Legacy'
 	},
-
 	'eslint-config-commonjs': {
 		name: 'ES2017, CommonJS',
 		es2017: true,
@@ -16,46 +15,46 @@ module.exports = {
 		esmodules: true
 	},
 
-	'eslint-config-commonjs-stage2': {
-		name: 'ES2017, CommonJS',
-		stage2: true,
-		es2017: true,
-		commonjs: true
-	},
-	'eslint-config-esmodules-stage2': {
-		name: 'ES2017, ES Modules',
-		stage2: true,
-		es2017: true,
-		esmodules: true
-	},
+	// 'eslint-config-commonjs-stage2': {
+	// 	name: 'ES2017, CommonJS',
+	// 	stage2: true,
+	// 	es2017: true,
+	// 	commonjs: true
+	// },
+	// 'eslint-config-esmodules-stage2': {
+	// 	name: 'ES2017, ES Modules',
+	// 	stage2: true,
+	// 	es2017: true,
+	// 	esmodules: true
+	// },
 
-	'eslint-config-commonjs-react': {
-		name: 'ES2017, CommonJS, React, JSX',
-		es2017: true,
-		commonjs: true,
-		react: true
-	},
-	'eslint-config-esmodules-react': {
-		name: 'ES2017, ES Modules, React, JSX',
-		es2017: true,
-		esmodules: true,
-		react: true
-	},
+	// 'eslint-config-commonjs-react': {
+	// 	name: 'ES2017, CommonJS, React, JSX',
+	// 	es2017: true,
+	// 	commonjs: true,
+	// 	react: true
+	// },
+	// 'eslint-config-esmodules-react': {
+	// 	name: 'ES2017, ES Modules, React, JSX',
+	// 	es2017: true,
+	// 	esmodules: true,
+	// 	react: true
+	// },
 
-	'eslint-config-commonjs-react-stage2': {
-		name: 'ES2017, CommonJS, React, JSX',
-		stage2: true,
-		es2017: true,
-		commonjs: true,
-		react: true
-	},
-	'eslint-config-esmodules-react-stage2': {
-		name: 'ES2017, ES Modules, React, JSX',
-		stage2: true,
-		es2017: true,
-		esmodules: true,
-		react: true
-	},
+	// 'eslint-config-commonjs-react-stage2': {
+	// 	name: 'ES2017, CommonJS, React, JSX',
+	// 	stage2: true,
+	// 	es2017: true,
+	// 	commonjs: true,
+	// 	react: true
+	// },
+	// 'eslint-config-esmodules-react-stage2': {
+	// 	name: 'ES2017, ES Modules, React, JSX',
+	// 	stage2: true,
+	// 	es2017: true,
+	// 	esmodules: true,
+	// 	react: true
+	// },
 	'eslint-config-typescript': {
 		name: 'ES2017, ES Modules, Typescript',
 		es2017: true,
@@ -63,4 +62,12 @@ module.exports = {
 		esmodules: true,
 		typescript: true
 	}
+	// 'eslint-config-typescript-jsx': {
+	// 	name: 'ES2017, ES Modules, Typescript',
+	// 	es2017: true,
+	// 	commonjs: true,
+	// 	esmodules: true,
+	// 	typescript: true,
+	// 	jsx: true
+	// }
 };
