7c1d1000033e9175dca138e8364d4b8abe58e785
Cecile Muller <git@wildpeaks.fr>
2019-06-25 04:08:51 +0200

    Muted rule `@typescript-eslint/restrict-plus-operands` in a few fixtures where it's not relevant to the test

--- test/fixtures/unused_param_with_underscore_first.js
+++ test/fixtures/unused_param_with_underscore_first.js
@@ -1,6 +1,7 @@
 /* global dummy */
 /* eslint-disable strict */
 /* eslint-disable space-before-blocks */
+/* eslint-disable @typescript-eslint/restrict-plus-operands */
 'use strict';
 
 function myfunction(_param1, param2){
