0f30f0d977c09b1f05fa98ea7b3bab0822106e8c
Cecile Muller <cecilemuller@users.noreply.github.com>
2020-05-14 09:49:25 +0200

    feat: disable rule `no-process-exit`

--- src/build.js
+++ src/build.js
@@ -217,7 +217,7 @@ function getEslintSettings({commonjs, es2017, esmodules, typescript}) {
 			"no-new-require": "off",
 			"no-path-concat": "off",
 			"no-process-env": "off",
-			"no-process-exit": "error",
+			"no-process-exit": "off",
 			"no-restricted-modules": "off",
 			"no-sync": "error",
 
