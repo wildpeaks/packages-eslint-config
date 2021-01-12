f2eb19c2bc75a4b2a1908786c440178949fca93f
Cecile Muller <cecilemuller@users.noreply.github.com>
2021-01-12 17:24:23 +0100

    test: check generated packages can be require()

---
+++ test/packages.test.js
@@ -0,0 +1,59 @@
+/* eslint-env node, mocha */
+/* eslint-disable prefer-arrow-callback */
+"use strict";
+const {strictEqual} = require("assert");
+const {existsSync, readFileSync} = require("fs");
+const {join} = require("path");
+const packagesFolder = join(process.cwd(), "packages");
+
+function runTest(id) {
+	it(id, function () {
+		const folder = join(packagesFolder, id);
+		strictEqual(existsSync(folder), true, "Folder exists");
+
+		let throws = false;
+		let packageJson;
+		try {
+			packageJson = JSON.parse(readFileSync(join(folder, "package.json"), "utf8"));
+		} catch (e) {
+			throws = true;
+		}
+		strictEqual(throws, false, "package.json can be read");
+		strictEqual(typeof packageJson, "object", "package is an Object");
+		strictEqual(packageJson.name, `@wildpeaks/eslint-config-${id}`, "package.name");
+		strictEqual(packageJson.main, "index.js", "package.main");
+
+		throws = false;
+		try {
+			readFileSync(join(folder, "index.js"), "utf8");
+		} catch (e) {
+			throws = true;
+		}
+		strictEqual(throws, false, "index.js can be read");
+
+		throws = false;
+		try {
+			readFileSync(join(folder, "README.md"), "utf8");
+		} catch (e) {
+			throws = true;
+		}
+		strictEqual(throws, false, "README.md can be read");
+
+		throws = false;
+		let settings;
+		try {
+			settings = require(`../packages/${id}`);
+		} catch (e) {
+			throws = true;
+		}
+		strictEqual(throws, false, "Package can be required");
+		strictEqual(typeof settings, "object", "Package exports an Object");
+		strictEqual(typeof settings.rules, "object", "Package contains rules");
+	});
+}
+
+describe("require()", function () {
+	runTest("legacy");
+	runTest("commonjs");
+	runTest("typescript");
+});
