4ea663c93b22f9e88c1165cd760b09285c8050e4
Cecile Muller <cecilemuller@users.noreply.github.com>
2021-10-08 13:39:17 +0200

    chore: add a js version of Prettier class fixture

---
+++ test/prettier/class_js/expected.js
@@ -0,0 +1,26 @@
+/* eslint-env node */
+class MyClass1 {}
+
+class MyClass2 {
+	constructor() {
+		console.log("Hello");
+	}
+	mymethod1() {
+		console.log(this.myproperty);
+	}
+	static mymethod2() {
+		console.log(typeof window);
+	}
+}
+
+class MyClass3 {
+	constructor() {
+		console.log("Hello");
+	}
+	mymethod1() {
+		console.log(this.myproperty);
+	}
+	static mymethod2() {
+		console.log(typeof window);
+	}
+}
